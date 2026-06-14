5ff9b097757443074478b143c6bbaec22225a6af
ArooiD <125826490+ArooiD@users.noreply.github.com>
2026-06-14 17:51:07 +0300

    Add production queue models

---
+++ src/economy/productionTypes.ts
@@ -0,0 +1,24 @@
+import type Phaser from 'phaser';
+import type { BuildingKind } from './buildingTypes';
+import type { UnitKind } from '../units/unitTypes';
+
+export type Building = {
+  id: number;
+  kind: BuildingKind;
+  world: Phaser.Math.Vector2;
+  sprite: Phaser.GameObjects.Rectangle;
+  label: Phaser.GameObjects.Text;
+  completed: boolean;
+  progressMs: number;
+  buildTimeMs: number;
+  hitpoints: number;
+  selected: boolean;
+  queue: ProductionOrder[];
+};
+
+export type ProductionOrder = {
+  id: number;
+  unitKind: UnitKind;
+  progressMs: number;
+  trainTimeMs: number;
+};
